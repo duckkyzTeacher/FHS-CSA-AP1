546ab0e01a793f75894f7e060b988fdae9221830
duckkyz <benrc_johnson@yahoo.com>
2024-10-27 19:55:54 -0700

    Initial Commit
    Still need to review tests and test autotesting/tests.

---
+++ src/main/java/AP1.java
@@ -0,0 +1,153 @@
+// AP1.java
+public class AP1 {
+    
+    /**
+     * Given an array of scores, return true if each score is equal or greater
+     * than the one before.
+     */
+    public boolean scoresIncreasing(int[] scores) {
+        return false;
+    }
+
+    /**
+     * Return true if the array contains 100 twice in a row.
+     */
+    public boolean scores100(int[] scores) {
+        return false;
+    }
+
+    /**
+     * Return true if there are three scores next to each other that are all
+     * within 2 of each other.
+     */
+    public boolean scoresClump(int[] scores) {
+        return false;
+    }
+
+    /**
+     * Return the average of the middle two scores in the array.
+     */
+    public int scoresAverage(int[] scores) {
+        return 0;
+    }
+
+    /**
+     * Given an array of words, return the count of the number of words of the
+     * given length.
+     */
+    public int wordsCount(String[] words, int len) {
+        return 0;
+    }
+
+    /**
+     * Given an array of words, return a new array with the first n words.
+     */
+    public String[] wordsFront(String[] words, int n) {
+        return new String[0];
+    }
+
+    /**
+     * Return a new list of strings without the strings of a given length.
+     */
+    public java.util.List<String> wordsWithoutList(String[] words, int len) {
+        return new java.util.ArrayList<>();
+    }
+
+    /**
+     * Given a positive int, return true if it has a 1 in it.
+     */
+    public boolean hasOne(int n) {
+        return false;
+    }
+
+    /**
+     * Given a positive int, return true if every digit divides the number evenly.
+     */
+    public boolean dividesSelf(int n) {
+        return false;
+    }
+
+    /**
+     * Return an array of the first n even numbers from the given array.
+     */
+    public int[] copyEvens(int[] nums, int count) {
+        return new int[0];
+    }
+
+    /**
+     * Return an array of the first n "endy" numbers from the array.
+     */
+    public int[] copyEndy(int[] nums, int count) {
+        return new int[0];
+    }
+
+    /**
+     * Return the number of times elements in nums1 match elements in nums2 at the same position.
+     */
+    public int matchUp(int[] nums1, int[] nums2) {
+        return 0;
+    }
+
+    /**
+     * Return the score based on the scoring rules.
+     */
+    public int scoreUp(String[] key, String[] answers) {
+        return 0;
+    }
+
+    /**
+     * Return a new array without the strings of the given length.
+     */
+    public String[] wordsWithout(String[] words, int len) {
+        return new String[0];
+    }
+
+    /**
+     * Return the sum of the largest two scores in the array that are special.
+     */
+    public int scoresSpecial(int[] a, int[] b) {
+        return 0;
+    }
+
+    /**
+     * Given an array of heights, return the sum of the differences for each adjacent pair.
+     */
+    public int sumHeights(int[] heights, int start, int end) {
+        return 0;
+    }
+
+    /**
+     * Like sumHeights, but for each step up (as opposed to down) add double.
+     */
+    public int sumHeights2(int[] heights, int start, int end) {
+        return 0;
+    }
+
+    /**
+     * Given an array of heights, return the number of "big" steps.
+     */
+    public int bigHeights(int[] heights, int start, int end) {
+        return 0;
+    }
+
+    /**
+     * Return 1 if a is greater, -1 if b is greater, and 0 if they are equal.
+     */
+    public int userCompare(String aName, int aId, String bName, int bId) {
+        return 0;
+    }
+
+    /**
+     * Return an array containing the first n elements from the sorted merge of arrays.
+     */
+    public String[] mergeTwo(String[] a, String[] b, int n) {
+        return new String[0];
+    }
+
+    /**
+     * Return the number of common elements in the sorted arrays.
+     */
+    public int commonTwo(String[] a, String[] b) {
+        return 0;
+    }
+}
