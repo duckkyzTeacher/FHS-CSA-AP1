db0f816f420ea294666dcc0932fa01f03f84c262
Benjamin Johnson <bjohnson@fcusd.org>
2024-11-05 09:11:10 -0800

    Update AP1.java

--- src/main/java/AP1.java
+++ src/main/java/AP1.java
@@ -125,7 +125,7 @@ public class AP1 {
      * Given an array of strings, return a new array without the strings that are equal to the target string. 
      * One approach is to count the occurrences of the target string, make a new array of the correct length, and then copy over the correct strings.
      */
-    public String[] wordsWithout(String[] words, int len) {
+    public String[] wordsWithout(String[] words, String target) {
         return new String[0];
     }
 
